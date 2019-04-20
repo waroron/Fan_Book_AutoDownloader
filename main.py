ede53a419edce897f315fda670775dc7d320addc
AnominP <ex.gonw122@gmail.com>
2019-04-20 18:39:56 +0900

    add some codes to main to enable to download all keywords file

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ def get_book_data_from_smart(thumb_flag=True):
         q.append(('type', '1'))
         q.append(('keyword', keyword))
         smart.get_book_data_from_secified_query(query=q, csv_name=csv_name, dir_name=dir_name, thumb_dir=thumb_dir)
+
+
+if __name__ == '__main__':
+    get_book_data_from_smart(thumb_flag=True)
